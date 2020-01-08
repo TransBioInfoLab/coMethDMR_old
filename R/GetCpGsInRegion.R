0d747617d4d22254133b0ae3a3357ad918a8ec73
Tiago Chedraoui Silva <tiagochst@gmail.com>
2020-01-08 13:55:49 -0500

    Improving speed in getCpgsinRegion

--- R/GetCpGsInRegion.R
+++ R/GetCpGsInRegion.R
@@ -1,4 +1,3 @@
-
 #' Extract probe IDs for CpGs located in a genomic region
 #'
 #' @param regionName_char character string with location information for one region in
@@ -8,14 +7,17 @@
 #' @return vector of CpG probe IDs mapped to the genomic region
 #' @export
 #'
-#' @importFrom stats lm
+#' @importFrom tidyr separate %>%
+#' @importFrom GenomicRanges makeGRangesFromDataFrame
+#' @importFrom IRanges subsetByOverlaps
 #'
 #' @examples
 #'    GetCpGsInRegion(
 #'      regionName_char = "chr22:18267969-18268249",
 #'      arrayType = "450k"
 #'    )
-GetCpGsInRegion <- function(regionName_char, arrayType = c("450k","EPIC")){
+GetCpGsInRegion <- function(regionName_char,
+                            arrayType = c("450k","EPIC")){
 
 
   arrayType <- match.arg(arrayType)
@@ -28,26 +30,86 @@ GetCpGsInRegion <- function(regionName_char, arrayType = c("450k","EPIC")){
            CpGlocations_df = IlluminaHumanMethylationEPICanno.ilm10b2.hg19::Locations
          }
   )
-
+  CpGlocations.gr <- CpGlocations_df %>%
+    makeGRangesFromDataFrame(end.field = "pos",
+                             start.field = "pos",
+                             ignore.strand = TRUE)
 
   ### Split the region name in chr and positions ###
-  split1 <- unlist(strsplit(regionName_char, ":"))
-  split2 <- unlist(strsplit(split1[2], "-"))
-  chr <- split1[1]
-  start <- split2[1]
-  end <- split2[2]
+  gr <- regionName_char %>%
+    as.data.frame %>%
+    separate(col = ".", into = c("seqnames","start","end")) %>%
+    makeGRangesFromDataFrame()
 
-
-  ### Subset the location Data Frame  ###
-  # Remove S4 class DataFrame
-  CpGlocations_df <- as.data.frame(CpGlocations_df)
-  CpGlocations_df$cpg <- row.names(CpGlocations_df)
-  row.names(CpGlocations_df) <- NULL
-  chr_df <- CpGlocations_df[which(CpGlocations_df$chr == chr), ]
-  CpGs_df <- chr_df[which(chr_df$pos >= as.integer(start) &
-    chr_df$pos <= as.integer(end)), ]
-
-
-  OrderCpGsByLocation(CpGs_df$cpg, arrayType = arrayType, output = "vector")
-
+  subsetByOverlaps(CpGlocations.gr, gr) %>% sort(ignore.strand = TRUE)  %>% names
 }
+
+#' Extract probe IDs for CpGs located in a genomic region
+#'
+#' @param regionName_char character string with location information for one region in
+#'    this format: "chrxx:xxxxxx-xxxxxx"
+#' @param arrayType Type of array, 450k or EPIC
+#' @param nCores_int Number of computing cores to be used when executing code
+#'    in parallel. Defaults to 1 (serial computing).
+#' @param ... Dots for additional arguments passed to the cluster constructor.
+#'    See \code{\link{CreateParallelWorkers}} for more information.
+#'
+#' @return vector of CpG probe IDs mapped to the genomic region
+#' @export
+#'
+#' @importFrom tidyr separate %>%
+#' @importFrom GenomicRanges makeGRangesFromDataFrame
+#' @importFrom IRanges subsetByOverlaps
+#'
+#' @examples
+#'    GetCpGsInRegion(
+#'      regionName_char = "chr22:18267969-18268249",
+#'      arrayType = "450k"
+#'    )
+GetCpGsInAllRegion <- function(regionName_char,
+                               arrayType = c("450k","EPIC"),
+                               nCores_int = 1L,
+                               ...){
+
+
+  arrayType <- match.arg(arrayType)
+
+  switch(arrayType,
+         "450k" = {
+           CpGlocations_df = IlluminaHumanMethylation450kanno.ilmn12.hg19::Locations
+         },
+         "EPIC" = {
+           CpGlocations_df = IlluminaHumanMethylationEPICanno.ilm10b2.hg19::Locations
+         }
+  )
+  CpGlocations.gr <- CpGlocations_df %>%
+    makeGRangesFromDataFrame(end.field = "pos",
+                             start.field = "pos",
+                             ignore.strand = TRUE)
+
+  ### Split the region name in chr and positions ###
+  message("Get regions from name")
+  regions.df <- regionName_char %>%
+    as.data.frame %>%
+    separate(col = ".", into = c("seqnames","start","end"))
+    regions.gr <- regions.df %>% makeGRangesFromDataFrame()
+    regions.list <- regions.df %>% makeGRangesListFromDataFrame()
+    names(regions.list) <- regionName_char
+
+    CpGlocations.gr <- subsetByOverlaps(CpGlocations.gr, regions.gr)  %>%
+      sort(ignore.strand = TRUE)
+
+  message("Iterating over regions... this may take a while")
+
+  cluster <- CreateParallelWorkers(nCores_int, ...)
+  results <- bplapply(
+    regions.list,
+    function(x) {
+      subsetByOverlaps(CpGlocations.gr, x) %>%
+        names
+    },  BPPARAM = cluster
+  )
+  results[regionName_char]
+}
+
+
