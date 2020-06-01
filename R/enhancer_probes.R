5b82af547f37cd639647c84e8d63a5d4628d2f50
Tiago <tiagochst@gmail.com>
2020-06-01 10:52:03 -0400

    update

---
+++ R/enhancer_probes.R
@@ -0,0 +1,8 @@
+#' Enhancer HM450 probes
+#'
+#' @description  list of HM450 probes names defined as enhancers
+#'
+#' @format A list of HM450 probes names
+#'
+#' @source Illumina array
+"enhancer.probes"
